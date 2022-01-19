32c4cd4b065288d078f86bb57518306f6b7676ad
Ramon Martinez <ramon.martinez@webfg.com>
2022-01-19 17:54:23 +0100

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+  heroes: string[] = ['Spiderman', 'Ironman', 'Batman'];
+  borrado: string = '';
+
+  borrarHeroe():void{
+    this.borrado = this.heroes.shift() || '';
+  }
+
+}
